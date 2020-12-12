71c71fad0a99bb54c5051c1f704ff3321c449885
joshsrai <joshsrai@gmail.com>
2020-12-12 18:20:22 -0500

    created weather_posts_controller and some routes

---
+++ app/controllers/weather_posts_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+    get '/login' do
+
+    end
+
+    post '/login' do
+
+    end
+
+    get '/signup' do
+
+    end
+
+    post '/users' do
+
+    end
+
+    get '/users/:id' do
+
+    end
+
+    get '/logout' do
+
+    end
+end
